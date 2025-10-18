fa07059a430d496cd21e6f9889e50a41af5638b0
BasireddyPavanKumarReddy <134392873+BasireddyPavanKumarReddy@users.noreply.github.com>
2025-10-18 23:31:45 +0530

    Added index.html and script.js with basic webpage and JS alert

---
+++ Script1.js
@@ -0,0 +1,6 @@
+// script.js
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript file loaded successfully!");
+    alert("Welcome to MyWebProject — powered by Visual Studio 2022!");
+});
+// JavaScript source code
